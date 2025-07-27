ddb0fa821e7f46354b12838dc9449953676b966d
n0o0b090lv <ernestspursw@gmail.com>
2025-07-27 16:40:39 +0300

    smaller range

--- Scripts/free_thinking_juice.gd
+++ Scripts/free_thinking_juice.gd
@@ -222,7 +222,7 @@ func cached_assign():
 func _physics_process(delta: float) -> void:
 	var global_mouse_pos=get_viewport().get_camera_2d().get_global_mouse_position()
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and Global.hands_free:
-		if global_mouse_pos.distance_to(global_position)<75:
+		if global_mouse_pos.distance_to(global_position)<25:
 			var aangle=atan2(global_mouse_pos.y-global_position.y,global_mouse_pos.x-global_position.x)
 			apply_force(Vector2(-cos(aangle),-sin(aangle))*delta*1200)
 	if "Accelerating" in liquid_data[ID]:
